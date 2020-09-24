b944226b3361df9b9f13cd3618d14e0f4689ad69
Agustin Bettati <bettatiagustin@gmail.com>
2020-09-24 16:39:22 -0300

    APIMF-2499: remove raml as known vendor in async plugin

---
+++ amf-client/shared/src/test/scala/amf/core/registries/WebApiPluginsRegistryTest.scala
@@ -0,0 +1,16 @@
+package amf.core.registries
+import amf.plugins.document.webapi.{Async20Plugin, Raml10Plugin}
+import org.scalatest.FunSuite
+import org.scalatest.Matchers._
+
+class WebApiPluginsRegistryTest extends FunSuite {
+
+  test("obtain raml plugin from registry when raml and async plugins are registered") {
+
+    AMFPluginsRegistry.registerDocumentPlugin(Async20Plugin)
+    AMFPluginsRegistry.registerDocumentPlugin(Raml10Plugin)
+
+    val plugins = AMFPluginsRegistry.documentPluginForVendor(Raml10Plugin.vendors.head)
+    plugins.head shouldBe Raml10Plugin
+  }
+}
